6aeca4dadcb61ce8968c60218150383c36e33ce5
dede-irwanto <id.dedeirwanto@gmail.com>
2023-09-06 15:09:39 +0800

    Belajar Java Stream

---
+++ src/test/java/programmer/zaman/now/stream/CreateStreamTest.java
@@ -0,0 +1,44 @@
+package programmer.zaman.now.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class CreateStreamTest {
+    @Test
+    void testEmptyOrSingleStream() {
+        Stream<String> emptyStream = Stream.empty();
+        emptyStream.forEach(data -> {
+            System.out.println(data);
+        });
+
+        Stream<String> oneStream = Stream.of("Dede");
+        oneStream.forEach(data -> {
+            System.out.println(data);
+        });
+
+        String data = "Irwanto";
+        Stream<String> emptyOrNotStream = Stream.ofNullable(data);
+        emptyOrNotStream.forEach(item -> {
+            System.out.println(item);
+        });
+    }
+
+    @Test
+    void testCreateStreamFromArray() {
+        Stream<String> arrayStream = Stream.of("Dede", "Irwanto");
+        arrayStream.forEach(data -> {
+            System.out.println(data);
+        });
+
+        Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5);
+        integerStream.forEach(System.out::println);
+
+        String[] array = new String[] {
+                "Dede", "Irwanto"
+        };
+        Stream<String> streamFromArray = Arrays.stream(array);
+        streamFromArray.forEach(System.out::println);
+    }
+}
